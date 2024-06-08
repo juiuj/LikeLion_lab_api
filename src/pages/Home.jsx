fbd9b40c7e3febe895c989d4c349647e6a5f3956
dev-joohee <dev.joohee0916@gmail.com>
2024-06-08 21:16:32 +0900

    Homepage 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import MyButton from '../Component/MyButton'
 
 const Home = () => {
+
+  const IsLogged= true
+  const username='홍길동'
+
   return (
-    <div>Home</div>
+    <HomeBox>
+    {IsLogged ? 
+    <div>
+      <h1>{username}님 반갑습니다!</h1>
+      <LogoutWrap>
+        <MyButton>로그아웃</MyButton>
+      </LogoutWrap>
+    </div>
+    :
+    <h1>지금당장 로그인!!</h1>}
+    </HomeBox>
   )
 }
+
+const HomeBox=styled('div')`
+  margin-top: 150px;
+  text-align: center;
+`
+
+const LogoutWrap=styled('div')`
+  width:300px;
+  margin: auto;
+`
 
 export default Home
